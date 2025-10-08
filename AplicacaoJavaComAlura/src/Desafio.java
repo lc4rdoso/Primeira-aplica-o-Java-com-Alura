808536f5793d0837586d7c708dd54970c1b7ff75
lc4rdoso <196863577+lc4rdoso@users.noreply.github.com>
2025-10-07 22:50:53 -0300

    Adicionando senha as requisições

--- AplicacaoJavaComAlura/src/Desafio.java
+++ AplicacaoJavaComAlura/src/Desafio.java
@@ -7,9 +7,10 @@ public class Desafio {
         Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
         String nomeUser, senhaUser;
+        double saldo = 0.0;
         char opcao;
         String menuDeOpcoes = """
-                Olá, nos diga o que você deseja fazer:
+                Nos diga o que você deseja fazer:
                 (D) Depositar
                 (E) Extrato
                 (S) Sacar
@@ -17,39 +18,68 @@ public class Desafio {
                 """;
         int numConta;
         System.out.println("Vamos abrir sua conta Digite seu Nome:");
-        nomeUser= input.nextLine();
+        nomeUser = input.nextLine();
         System.out.println("Qual tipo de conta? Digite C para corrente ou P para poupança");
         char tipoConta = input.next().charAt(0);
         numConta = new Random().nextInt(5000);
-        System.out.println("Olá " + nomeUser + " sua conta é "+ numConta);
+        System.out.println("Olá " + nomeUser + " sua conta é " + numConta);
+        System.out.println("Agora cadastre uma senha ");
+        input.nextLine();
+        senhaUser = input.nextLine();
         System.out.println("Deseja acessar sua conta ou sair do app? digite A para acessar ou S para sair");
         char acessarConta = input.next().charAt(0);
-        if (acessarConta =='A'){
-            System.out.println(menuDeOpcoes);
-            opcao = input.next().charAt(0);
-            while(opcao!= 'X' ) {
-                switch (opcao) {
-                    case 'D':
+        if (acessarConta == 'A') {
+            System.out.println("Para continuar digite sua senha");
+            input.nextLine();
+            String tentativaDeSenha = input.nextLine();
+            if ( senhaUser.equals(tentativaDeSenha)) {
+                System.out.println(menuDeOpcoes);
+                opcao = input.next().charAt(0);
+                while (opcao != 'X') {
+                    switch (opcao) {
+                        case 'D':
+                            System.out.println("Para continuar digite sua senha");
+                            tentativaDeSenha = input.nextLine();
+                            if ( senhaUser.equals(tentativaDeSenha)) {
+                                System.out.println("Olá " + nomeUser + " seu saldo atual é de: " + saldo + "quanto deseja depositar?");
+                                saldo = saldo + input.nextDouble();
+                                System.out.println("Operação efetuada com sucesso.\n" + menuDeOpcoes);
+                                opcao = input.next().charAt(0);
+                            }
 
-                        break;
-                    case 'E':
-                        break;
-                    case 'S':
-                        break;
-                    case  'X':
-                        break;
-                    default:
+                            break;
+                        case 'E':
+                            System.out.println("Olá " + nomeUser + " seu saldo atual é de: " + saldo);
+                            System.out.println("Operação efetuada com sucesso.\n " + menuDeOpcoes);
+                            opcao = input.next().charAt(0);
+                            ;
+                            break;
+                        case 'S':
+                            if (saldo <= 0.0) {
+                                System.out.println("Você não possui fundos suficientes para executar essa operação\n" + menuDeOpcoes);
+                            }
+                            System.out.println("Olá " + nomeUser + " seu saldo atual é de: " + saldo + "quanto deseja sacar");
+                            saldo = saldo - input.nextDouble();
+                            System.out.println("Operação efetuada com sucesso.\n" + menuDeOpcoes);
+                            opcao = input.next().charAt(0);
+                            ;
+                            break;
+                        case 'X':
+                            System.out.println("Você escolheu sair, até logo.");
+                            break;
+                        default:
 
-                        System.out.println(menuDeOpcoes);
+                            System.out.println("OPÇÃO INVALIDA \n" + menuDeOpcoes);
+                            opcao = input.next().charAt(0);
+                            ;
 
+                    }
                 }
+
             }
-        }else {
-            System.out.println("Você escolheu sair, até logo.");
+        } else {
+
         }
-
-
-
 
 
     }
